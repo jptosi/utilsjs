af243321f2993d497c4f0edb03cd20503974057e
jptosi <jp.tosi@gmail.com>
2021-02-01 14:54:28 +0100

    remove scale

--- tools.js
+++ tools.js
@@ -34,8 +34,8 @@ function lerp(start, target, step){
 
 /** mapRange
     n : value on scale1
-    start1 : origine on scale 1 - end1 : end of scale1
-    start2 : origine on scale 2 - end2 : end of scale2
+    start1 : origine on range1 - end1 : end of range1
+    start2 : origine on range2 - end2 : end of range2
 **/
 function mapRange(n, start1, end1, start2, end2){
     return (n - start1) / (end1 - start1) * (end2 - start2) + start2;
